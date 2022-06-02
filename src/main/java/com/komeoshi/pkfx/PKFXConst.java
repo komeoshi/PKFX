d7953c99b4051da86a8446ed65967979d34c870f
komeoshi <komeoshi@gmail.com>
2022-06-03 00:05:32 +0900

    Changes

--- src/main/java/com/komeoshi/pkfx/PKFXConst.java
+++ src/main/java/com/komeoshi/pkfx/PKFXConst.java
@@ -27,7 +27,7 @@ public class PKFXConst {
     /**
      * 売買単位.
      */
-    public static final String DEFAULT_UNIT = "250000";
+    public static final String DEFAULT_UNIT = "45000";
 
     /**
      * シグナル点灯閾値.
