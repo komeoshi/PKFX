4a2aed972b850e42706f6b43b2d509027feade4f
komeoshi <komeoshi@gmail.com>
2022-06-14 17:55:52 +0900

    パラメータ調整

--- src/main/java/com/komeoshi/pkfx/PKFXMiniDataGCTrader.java
+++ src/main/java/com/komeoshi/pkfx/PKFXMiniDataGCTrader.java
@@ -106,7 +106,7 @@ public class PKFXMiniDataGCTrader {
 
     private Status losscut(RestTemplate restTemplate, PKFXFinderRestClient client, Status status, Candle openCandle, Candle candle) {
         // 小さくするとロスカットしやすくなる
-        double lossCutMag = 0.000075;
+        double lossCutMag = 0.000085;
 
         double lossCutRateBuy = openCandle.getMid().getC() * (1 - lossCutMag);
         double lossCutRateSell = openCandle.getMid().getC() * (1 + lossCutMag);
