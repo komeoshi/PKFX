62f13d2104a29be6a047b0e42648bfe9b1e29526
komeoshi <komeoshi@gmail.com>
2022-06-26 23:01:47 +0900

    Finderを実装

--- src/main/java/com/komeoshi/pkfx/PKFXMiniDataGCSimulator.java
+++ src/main/java/com/komeoshi/pkfx/PKFXMiniDataGCSimulator.java
@@ -97,7 +97,7 @@ public class PKFXMiniDataGCSimulator {
         for (int ii = 0; ii < candles.size(); ii++) {
             Candle candle = candles.get(ii);
 
-            if (candles.size() / 10 < ii &&
+            if (candles.size() / 20 < ii &&
                     (totalCount == 0 || diff < 0 || countWin < countLose)) {
                 // これ以上やっても無駄
                 break;
