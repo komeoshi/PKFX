eae1f15cd4eafc0ba205508285f50837e3af1e0f
komeoshi <komeoshi@gmail.com>
2022-06-27 01:45:58 +0900

    Finderを実装

--- src/main/java/com/komeoshi/pkfx/PKFXMiniDataGCFinder.java
+++ src/main/java/com/komeoshi/pkfx/PKFXMiniDataGCFinder.java
@@ -262,7 +262,7 @@ public class PKFXMiniDataGCFinder {
                                        Parameter parameter,
                                        long time) {
             completeCount++;
-            if (diff >= maxDiff) {
+            if (diff != 0.0 && diff >= maxDiff) {
                 maxDiff = diff;
                 maxDiffTotal = total;
                 maxDiffParameter = parameter;
