30ec248f294d16c510470b898046547d3ef64a35
komeoshi <komeoshi@gmail.com>
2022-06-27 17:38:58 +0900

    パラメータ調整

--- src/main/java/com/komeoshi/pkfx/dto/parameter/ParameterB$Adx.java
+++ src/main/java/com/komeoshi/pkfx/dto/parameter/ParameterB$Adx.java
@@ -17,7 +17,7 @@ public class ParameterB$Adx {
 
     public static List<Double> createParameters() {
         List<Double> parameters = new ArrayList<>();
-        for (int i = 10; i <= 40; i += 2) {
+        for (int i = 10; i <= 40; i += 1) {
             parameters.add((double) i);
         }
 
