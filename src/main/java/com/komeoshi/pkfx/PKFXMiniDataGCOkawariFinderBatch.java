c6b0dc26c889dc06ab39cea2bef20c4ba6e8dfef
komeoshi <komeoshi@gmail.com>
2022-07-02 01:34:11 +0900

    OkawariFinderを実装

---
+++ src/main/java/com/komeoshi/pkfx/PKFXMiniDataGCOkawariFinderBatch.java
@@ -0,0 +1,43 @@
+package com.komeoshi.pkfx;
+
+import com.komeoshi.pkfx.dto.parameter.Parameter;
+import com.komeoshi.pkfx.dto.parameter.okawari.OkawariParameter;
+import lombok.Getter;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Getter
+@Setter
+public class PKFXMiniDataGCOkawariFinderBatch {
+    private static final Logger log = LoggerFactory.getLogger(PKFXMiniDataGCOkawariFinderBatch.class);
+
+    public static void main(String[] args) {
+        PKFXMiniDataGCOkawariFinderBatch batch = new PKFXMiniDataGCOkawariFinderBatch();
+        batch.execute();
+    }
+
+    public void execute() {
+
+        OkawariParameter maxParameter = OkawariParameter.getOkawariParameter();
+        double maxDiff = -999.0;
+        while (true) {
+            PKFXMiniDataGCOkawariFinder finder = new PKFXMiniDataGCOkawariFinder();
+            finder.setBatch(true);
+            finder.setExecuteMaxSize(1500);
+            finder.setDefaultParameter(maxParameter);
+            finder.setMaxDiffAllTheTime(maxDiff);
+
+            finder.execute();
+
+            double tmpMaxDiff = finder.getMaxDiff();
+            if (maxDiff < tmpMaxDiff) {
+                maxDiff = tmpMaxDiff;
+                maxParameter = finder.getMaxDiffParameter();
+            }
+            log.info("-- " + maxDiff + " ---------------------------------------------------------");
+        }
+    }
+}
+
+
--- src/main/java/com/komeoshi/pkfx/dto/parameter/okawari/OkawariParameter.java
+++ src/main/java/com/komeoshi/pkfx/dto/parameter/okawari/OkawariParameter.java
@@ -18,8 +18,8 @@ public class OkawariParameter {
         parameter1.setParamF(new OkawariParameterF$Rsi(20));
         parameter1.setParamG(new OkawariParameterG$Bband(0.380));
         parameter1.setParamH(new OkawariParameterH$Bband(0.097));
-        parameter1.setParamI(new OkawariParameterI$Sig(0.004));
-        parameter1.setParamJ(new OkawariParameterJ$Atr(0.016));
+        parameter1.setParamI(new OkawariParameterI$Sig(1.0/1000));
+        parameter1.setParamJ(new OkawariParameterJ$Atr(12.0/1000));
 
         return parameter1;
     }
