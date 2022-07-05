ea735ef12ffbceef9cef0bfe051a5b4e75748a74
komeoshi <komeoshi@gmail.com>
2022-07-06 01:56:01 +0900

    リファクタリング

--- src/main/java/com/komeoshi/pkfx/restclient/PKFXSimulatorRestClient.java
+++ src/main/java/com/komeoshi/pkfx/restclient/PKFXSimulatorRestClient.java
@@ -31,18 +31,18 @@ public class PKFXSimulatorRestClient {
         LocalDateTime from = LocalDateTime.of(_from.getYear(), _from.getMonthValue(), _from.getDayOfMonth(), 0, 0, 0, 0);
         LocalDateTime fromTmp = from.plusHours(1);
 
-        List<String> days = new ArrayList<>();
+        List<String> mins = new ArrayList<>();
         while (from.isBefore(LocalDateTime.of(_to.getYear(), _to.getMonthValue(), _to.getDayOfMonth(), 0, 0, 0, 0))) {
             String dateFrom = from.format(DateTimeFormatter.ISO_DATE_TIME);
             String dateTo = fromTmp.format(DateTimeFormatter.ISO_DATE_TIME);
 
             String day = "from=" + dateFrom + ".000000000Z&to=" + dateTo + ".000000000Z";
-            days.add(day);
+            mins.add(day);
             from = from.plusHours(1);
             fromTmp = fromTmp.plusHours(1);
         }
 
-        return days;
+        return mins;
     }
 
     private List<String> getDays(LocalDate _from, LocalDate _to) {
